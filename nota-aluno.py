a52c49a4faf7bd7a049fd205e1de2ffa9016606d
gblbjj <oliveira992gabriel@gmail.com>
2019-07-07 17:32:28 -0300

    nota

---
+++ nota-aluno.py
@@ -0,0 +1,14 @@
+x = str(input('Ola bom dia! Qual seu nome: '))
+print('Ola', x, 'vamos ver sus nota agora!')
+n = int(input('digite sua nota para iniciar os calculos: '))
+
+media = 6
+
+if n >= 6:
+    print('Parabens', x, 'voce foi aprovado direto')
+elif n ==5:
+    print('sinto muito', x, 'voce esta de recuperacao mas ainda nao foi reprovado')
+else:
+    print('segundo nosso sistema', x, 'voce foi reprovado direto')
+
+print ('Obrigado', x, 'por utilizar nossos serviços BYE-BYE!')
